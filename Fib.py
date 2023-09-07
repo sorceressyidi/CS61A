fa0f5c742b3fa5a85b19e400b3b9f0805a95a0a6
Lily <sorceressyidi@163.com>
2023-09-07 21:47:28 +0800

    add  all_nums

--- Fib.py
+++ Fib.py
@@ -14,3 +14,14 @@ def print_sums(n):
                 return print_sums(n+k)
         return next_sum
 
+# Also Hard to think
+def all_nums(k):
+    def h(k, prefix):
+        if k == 0:
+            print(prefix)
+            return
+        h(k - 1, prefix * 10)
+        h(k - 1, prefix * 10 + 1)
+    h(k, 0)
+
+all_nums(10)
